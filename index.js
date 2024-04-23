1b642549e507a11c5c0a33c00ea90ff0f98f2663
Tom Bangle <tombangle@gmail.com>
2024-04-23 16:22:20 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+function titleCased() {
+    const tutorials = [
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
 
-const titleCased = () => {
-  return tutorials
+  const titleCasedTutorials = [] 
+    for (const tutorial of tutorials) {
+      const words = tutorial.split(" ");
+      const titleCasedName = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+      titleCasedTutorials.push(titleCasedName);
+    }
+    return titleCasedTutorials;
 }
+console.log(titleCased());
